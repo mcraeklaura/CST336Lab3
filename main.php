5b60f20aca7da6f4bb76ab2267ed121b1525087b
Laura <lmcrae@csumb.edu>
2016-09-16 20:02:17 +0000

    Add some arrays

--- main.php
+++ main.php
@@ -1,4 +1,7 @@
 <?php
+
+//Hearts, Diamonds, Spades, Clubs
+
 $deck = array(
     $kingH, $kingD, $kingS, $kingC,
     $queenH, $queenD, $queenS, $queenC,
@@ -14,6 +17,37 @@ $deck = array(
     $twoH, $twoD, $twoS, $twoC,
     $aceH, $aceD, $aceS, $aceC,
     );
+    
+    $kingH = array(
+        "value" => 13,
+        "img" => "",
+        );
+    $kingD = array(
+        "value" => 13,
+        "img" => "",
+        );
+    $kingS  = array(
+        "value" => 13,
+        "img" => "",
+        );
+    $kingC = array(
+        "value" => 13,
+        "img" => "",
+        );
+    
+    $queenH = array(
+        "value" => 12,
+        "img" => "",
+        );
+    $queenD = array(
+        "value" => 12,
+        "img" => "",
+        );
+    $queenS = array(
+        "value" => 12,
+        "img" => "",
+        );
+    
 ?>
 
 hello

fb430fba070ed65e1b85e8c977d21fda229528d4
Laura <lmcrae@csumb.edu>
2016-09-16 20:32:04 +0000

    Adds all card values

--- main.php
+++ main.php
@@ -1,54 +1,94 @@
-<?php
+<html>
+    <head>
+        
+    </head>
+    <body>
+    <?php
 
-//Hearts, Diamonds, Spades, Clubs
+    //Hearts, Diamonds, Spades, Clubs
 
-$deck = array(
-    $kingH, $kingD, $kingS, $kingC,
-    $queenH, $queenD, $queenS, $queenC,
-    $jackH, $jackD, $jackS, $jackC,
-    $tenH, $tenD, $tenS, $tenC,
-    $nineH, $nineD, $nineS, $nineC,
-    $eightH, $eightD, $eightS, $eightC,
-    $sevenH, $sevenD, $sevenS, $sevenC,
-    $sixH, $sixD, $sixS, $sixC,
-    $fiveH, $fiveD, $fiveS, $fiveC,
-    $fourH, $fourD, $fourS, $fourC,
-    $threeH, $threeD, $threeS, $threeC,
-    $twoH, $twoD, $twoS, $twoC,
-    $aceH, $aceD, $aceS, $aceC,
+    $deck = array(
+        $kingH, $kingD, $kingS, $kingC,
+        $queenH, $queenD, $queenS, $queenC,
+        $jackH, $jackD, $jackS, $jackC,
+        $tenH, $tenD, $tenS, $tenC,
+        $nineH, $nineD, $nineS, $nineC,
+        $eightH, $eightD, $eightS, $eightC,
+        $sevenH, $sevenD, $sevenS, $sevenC,
+        $sixH, $sixD, $sixS, $sixC,
+        $fiveH, $fiveD, $fiveS, $fiveC,
+        $fourH, $fourD, $fourS, $fourC,
+        $threeH, $threeD, $threeS, $threeC,
+        $twoH, $twoD, $twoS, $twoC,
+        $aceH, $aceD, $aceS, $aceC,
     );
     
-    $kingH = array(
-        "value" => 13,
-        "img" => "",
-        );
-    $kingD = array(
-        "value" => 13,
-        "img" => "",
-        );
-    $kingS  = array(
-        "value" => 13,
-        "img" => "",
-        );
-    $kingC = array(
-        "value" => 13,
-        "img" => "",
-        );
+    $kingH = array("value" => 13,"img" => "",);
+    $kingD = array("value" => 13,"img" => "",);
+    $kingS = array("value" => 13,"img" => "",);
+    $kingC = array("value" => 13,"img" => "",);
     
-    $queenH = array(
-        "value" => 12,
-        "img" => "",
-        );
-    $queenD = array(
-        "value" => 12,
-        "img" => "",
-        );
-    $queenS = array(
-        "value" => 12,
-        "img" => "",
-        );
+    $queenH = array("value" => 12,"img" => "",);
+    $queenD = array("value" => 12,"img" => "",);
+    $queenS = array("value" => 12,"img" => "",);
+    $queenC = array("value" => 12,"img" => "",);
+    
+    $jackH = array("value" => 11, "img" => "",);
+    $jackD = array("value" => 11, "img" => "",);
+    $jackS = array("value" => 11, "img" => "",);
+    $jackC = array("value" => 11, "img" => "",);
+    
+    $tenH = array("value" => 10, "img" => "",);
+    $tenD = array("value" => 10, "img" => "",);
+    $tenS = array("value" => 10, "img" => "",);
+    $tenC = array("value" => 10, "img" => "",);
+    
+    $nineH = array("value" => 9, "img" => "",); 
+    $nineD = array("value" => 9, "img" => "",);
+    $nineS = array("value" => 9, "img" => "",);
+    $nineC = array("value" => 9, "img" => "",);
+    
+    $eightH = array("value" => 8, "img" => "",);
+    $eightD = array("value" => 8, "img" => "",);
+    $eightS = array("value" => 8, "img" => "",);
+    $eightC = array("value" => 8, "img" => "",);
+    
+    $sevenH = array("value" => 7, "img" => "",);
+    $sevenD = array("value" => 7, "img" => "",);
+    $sevenS = array("value" => 7, "img" => "",);
+    $sevenC = array("value" => 7, "img" => "",);
+    
+    $sixH = array("value" => 6, "img" => "",);
+    $sixD = array("value" => 6, "img" => "",);
+    $sixS = array("value" => 6, "img" => "",);
+    $sixC = array("value" => 6, "img" => "",);
+    
+    $fiveH = array("value" => 5, "img" => "",);
+    $fiveD = array("value" => 5, "img" => "",);
+    $fiveS = array("value" => 5, "img" => "",);
+    $fiveC = array("value" => 5, "img" => "",);
+    
+    $fourH = array("value" => 4, "img" => "",);
+    $fourD = array("value" => 4, "img" => "",);
+    $fourS = array("value" => 4, "img" => "",);
+    $fourC = array("value" => 4, "img" => "",);
+    
+    $threeH = array("value" => 3, "img" => "",);
+    $threeD = array("value" => 3, "img" => "",);
+    $threeS = array("value" => 3, "img" => "",);
+    $threeC = array("value" => 3, "img" => "",);
+    
+    $twoH = array("value" => 2, "img" => "",);
+    $twoD = array("value" => 2, "img" => "",);
+    $twoS = array("value" => 2, "img" => "",);
+    $twoC = array("value" => 2, "img" => "",);
+    
+    $aceH = array("value" => 1, "img" => "",);
+    $aceD = array("value" => 1, "img" => "",);    
+    $aceS = array("value" => 1, "img" => "",);
+    $aceC = array("value" => 1, "img" => "",);
     
 ?>
-hello
+    </body>
+</html>
 
-hello

f77c0ecff4ef13a989d6ec54c7e72d89bf3125cd
Laura <lmcrae@csumb.edu>
2016-09-16 19:59:40 +0000

    Silver Jack Main

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php
+$deck = array(
+    $kingH, $kingD, $kingS, $kingC,
+    $queenH, $queenD, $queenS, $queenC,
+    $jackH, $jackD, $jackS, $jackC,
+    $tenH, $tenD, $tenS, $tenC,
+    $nineH, $nineD, $nineS, $nineC,
+    $eightH, $eightD, $eightS, $eightC,
+    $sevenH, $sevenD, $sevenS, $sevenC,
+    $sixH, $sixD, $sixS, $sixC,
+    $fiveH, $fiveD, $fiveS, $fiveC,
+    $fourH, $fourD, $fourS, $fourC,
+    $threeH, $threeD, $threeS, $threeC,
+    $twoH, $twoD, $twoS, $twoC,
+    $aceH, $aceD, $aceS, $aceC,
+    );
+?>
+
+hello

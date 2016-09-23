c0e80e8106576fcfd15183ff00c61c493aaae28c
Jessica <jesvega@csumb.edu>
2016-09-23 22:19:27 +0000

    randomly display player images, displays player hand, displays player score

--- main.php
+++ main.php
@@ -8,10 +8,10 @@
     $winner = array();
     
     $array = array(); $array1 = array(); $array2 = array(); $array3 = array();
-    $me = array("points" => 0, "hand" => $array);
-        $comp1 = array("points" => 0, "hand" => $array1);
-        $comp2 = array("points" => 0, "hand" => $array2);
-        $comp3 = array("points" => 0, "hand" => $array3);
+    $me = array("points" => 0, "hand" => $array, "img" => "<img src=\"PlayerImgs/player1.jpg\" alt=\"Player1\">");
+        $comp1 = array("points" => 0, "hand" => $array1, "img" => "<img src=\"PlayerImgs/player2.jpg\" alt=\"Player2\">");
+        $comp2 = array("points" => 0, "hand" => $array2,"img" => "<img src=\"PlayerImgs/player3.jpg\" alt=\"Player3\">") ;
+        $comp3 = array("points" => 0, "hand" => $array3,"img" => "<img src=\"PlayerImgs/player4.jpg\" alt=\"Player4\">") ;
     $players = array(
         $me,
         $comp1,
@@ -19,70 +19,70 @@
         $comp3,
         );
 
-    $kingH = array("value" => 13,"img" => "",);
-    $kingD = array("value" => 13,"img" => "",);
-    $kingS = array("value" => 13,"img" => "",);
-    $kingC = array("value" => 13,"img" => "",);
+    $kingH = array("value" => 13,"img" => "<img src=\"CardImgs/Hearts/kingH.png\" alt=\"CardImage\">",);
+    $kingD = array("value" => 13,"img" => "<img src=\"CardImgs/Diamonds/kingD.png\" alt=\"CardImage\">",);
+    $kingS = array("value" => 13,"img" => "<img src=\"CardImgs/Spades/kingS.png\" alt=\"CardImage\">",);
+    $kingC = array("value" => 13,"img" => "<img src=\"CardImgs/Clubs/kingC.png\" alt=\"CardImage\">",);
     
-    $queenH = array("value" => 12,"img" => "",);
-    $queenD = array("value" => 12,"img" => "",);
-    $queenS = array("value" => 12,"img" => "",);
-    $queenC = array("value" => 12,"img" => "",);
+    $queenH = array("value" => 12,"img" => "<img src=\"CardImgs/Hearts/queenH.png\" alt=\"CardImage\">",);
+    $queenD = array("value" => 12,"img" => "<img src=\"CardImgs/Diamonds/queenD.png\" alt=\"CardImage\">",);
+    $queenS = array("value" => 12,"img" => "<img src=\"CardImgs/Spades/queenS.png\" alt=\"CardImage\">",);
+    $queenC = array("value" => 12,"img" => "<img src=\"CardImgs/Clubs/queenC.png\" alt=\"CardImage\">",);
     
-    $jackH = array("value" => 11, "img" => "",);
-    $jackD = array("value" => 11, "img" => "",);
-    $jackS = array("value" => 11, "img" => "",);
-    $jackC = array("value" => 11, "img" => "",);
+    $jackH = array("value" => 11, "img" => "<img src=\"CardImgs/Hearts/jackH.png\" alt=\"CardImage\">",);
+    $jackD = array("value" => 11, "img" => "<img src=\"CardImgs/Diamonds/jackD.png\" alt=\"CardImage\">",);
+    $jackS = array("value" => 11, "img" => "<img src=\"CardImgs/Spades/jackS.png\" alt=\"CardImage\">",);
+    $jackC = array("value" => 11, "img" => "<img src=\"CardImgs/Clubs/jackC.png\" alt=\"CardImage\">",);
     
-    $tenH = array("value" => 10, "img" => "",);
-    $tenD = array("value" => 10, "img" => "",);
-    $tenS = array("value" => 10, "img" => "",);
-    $tenC = array("value" => 10, "img" => "",);
+    $tenH = array("value" => 10, "img" => "<img src=\"CardImgs/Hearts/tenH.png\" alt=\"CardImage\">",);
+    $tenD = array("value" => 10, "img" => "<img src=\"CardImgs/Diamonds/tenD.png\" alt=\"CardImage\">",);
+    $tenS = array("value" => 10, "img" => "<img src=\"CardImgs/Spades/tenS.png\" alt=\"CardImage\">",);
+    $tenC = array("value" => 10, "img" => "<img src=\"CardImgs/Clubs/tenC.png\" alt=\"CardImage\">",);
     
-    $nineH = array("value" => 9, "img" => "",); 
-    $nineD = array("value" => 9, "img" => "",);
-    $nineS = array("value" => 9, "img" => "",);
-    $nineC = array("value" => 9, "img" => "",);
+    $nineH = array("value" => 9, "img" => "<img src=\"CardImgs/Hearts/nineH.png\" alt=\"CardImage\">",); 
+    $nineD = array("value" => 9, "img" => "<img src=\"CardImgs/Diamonds/nineD.png\" alt=\"CardImage\">",);
+    $nineS = array("value" => 9, "img" => "<img src=\"CardImgs/Spades/nineS.png\" alt=\"CardImage\">",);
+    $nineC = array("value" => 9, "img" => "<img src=\"CardImgs/Clubs/nineC.png\" alt=\"CardImage\">",);
     
-    $eightH = array("value" => 8, "img" => "",);
-    $eightD = array("value" => 8, "img" => "",);
-    $eightS = array("value" => 8, "img" => "",);
-    $eightC = array("value" => 8, "img" => "",);
+    $eightH = array("value" => 8, "img" => "<img src=\"CardImgs/Hearts/eightH.png\" alt=\"CardImage\">",);
+    $eightD = array("value" => 8, "img" => "<img src=\"CardImgs/Diamonds/eightD.png\" alt=\"CardImage\">",);
+    $eightS = array("value" => 8, "img" => "<img src=\"CardImgs/Spades/eightS.png\" alt=\"CardImage\">",);
+    $eightC = array("value" => 8, "img" => "<img src=\"CardImgs/Clubs/eightC.png\" alt=\"CardImage\">",);
     
-    $sevenH = array("value" => 7, "img" => "",);
-    $sevenD = array("value" => 7, "img" => "",);
-    $sevenS = array("value" => 7, "img" => "",);
-    $sevenC = array("value" => 7, "img" => "",);
+    $sevenH = array("value" => 7, "img" => "<img src=\"CardImgs/Hearts/sevenH.png\" alt=\"CardImage\">",);
+    $sevenD = array("value" => 7, "img" => "<img src=\"CardImgs/Diamonds/sevenD.png\" alt=\"CardImage\">",);
+    $sevenS = array("value" => 7, "img" => "<img src=\"CardImgs/Spades/sevenS.png\" alt=\"CardImage\">",);
+    $sevenC = array("value" => 7, "img" => "<img src=\"CardImgs/Clubs/sevenC.png\" alt=\"CardImage\">",);
     
-    $sixH = array("value" => 6, "img" => "",);
-    $sixD = array("value" => 6, "img" => "",);
-    $sixS = array("value" => 6, "img" => "",);
-    $sixC = array("value" => 6, "img" => "",);
+    $sixH = array("value" => 6, "img" => "<img src=\"CardImgs/Hearts/sixH.png\" alt=\"CardImage\">",);
+    $sixD = array("value" => 6, "img" => "<img src=\"CardImgs/Diamonds/sixD.png\" alt=\"CardImage\">",);
+    $sixS = array("value" => 6, "img" => "<img src=\"CardImgs/Spades/sixS.png\" alt=\"CardImage\">",);
+    $sixC = array("value" => 6, "img" => "<img src=\"CardImgs/Clubs/sixC.png\" alt=\"CardImage\">",);
     
-    $fiveH = array("value" => 5, "img" => "",);
-    $fiveD = array("value" => 5, "img" => "",);
-    $fiveS = array("value" => 5, "img" => "",);
-    $fiveC = array("value" => 5, "img" => "",);
+    $fiveH = array("value" => 5, "img" => "<img src=\"CardImgs/Hearts/fiveH.png\" alt=\"CardImage\">",);
+    $fiveD = array("value" => 5, "img" => "<img src=\"CardImgs/Diamonds/fiveD.png\" alt=\"CardImage\">",);
+    $fiveS = array("value" => 5, "img" => "<img src=\"CardImgs/Spades/fiveS.png\" alt=\"CardImage\">",);
+    $fiveC = array("value" => 5, "img" => "<img src=\"CardImgs/Clubs/fiveC.png\" alt=\"CardImage\">",);
     
-    $fourH = array("value" => 4, "img" => "",);
-    $fourD = array("value" => 4, "img" => "",);
-    $fourS = array("value" => 4, "img" => "",);
-    $fourC = array("value" => 4, "img" => "",);
+    $fourH = array("value" => 4, "img" => "<img src=\"CardImgs/Hearts/fourH.png\" alt=\"CardImage\">",);
+    $fourD = array("value" => 4, "img" => "<img src=\"CardImgs/Diamonds/fourD.png\" alt=\"CardImage\">",);
+    $fourS = array("value" => 4, "img" => "<img src=\"CardImgs/Spades/fourS.png\" alt=\"CardImage\">",);
+    $fourC = array("value" => 4, "img" => "<img src=\"CardImgs/Clubs/fourC.png\" alt=\"CardImage\">",);
     
-    $threeH = array("value" => 3, "img" => "",);
-    $threeD = array("value" => 3, "img" => "",);
-    $threeS = array("value" => 3, "img" => "",);
-    $threeC = array("value" => 3, "img" => "",);
+    $threeH = array("value" => 3, "img" => "<img src=\"CardImgs/Hearts/threeH.png\" alt=\"CardImage\">",);
+    $threeD = array("value" => 3, "img" => "<img src=\"CardImgs/Diamonds/threeD.png\" alt=\"CardImage\">",);
+    $threeS = array("value" => 3, "img" => "<img src=\"CardImgs/Spades/threeS.png\" alt=\"CardImage\">",);
+    $threeC = array("value" => 3, "img" => "<img src=\"CardImgs/Clubs/threeC.png\" alt=\"CardImage\">",);
     
-    $twoH = array("value" => 2, "img" => "",);
-    $twoD = array("value" => 2, "img" => "",);
-    $twoS = array("value" => 2, "img" => "",);
-    $twoC = array("value" => 2, "img" => "",);
+    $twoH = array("value" => 2, "img" => "<img src=\"CardImgs/Hearts/twoH.png\" alt=\"CardImage\">",);
+    $twoD = array("value" => 2, "img" => "<img src=\"CardImgs/Diamonds/twoD.png\" alt=\"CardImage\">",);
+    $twoS = array("value" => 2, "img" => "<img src=\"CardImgs/Spades/twoS.png\" alt=\"CardImage\">",);
+    $twoC = array("value" => 2, "img" => "<img src=\"CardImgs/Clubs/twoC.png\" alt=\"CardImage\">",);
     
-    $aceH = array("value" => 1, "img" => "",);
-    $aceD = array("value" => 1, "img" => "",);    
-    $aceS = array("value" => 1, "img" => "",);
-    $aceC = array("value" => 1, "img" => "",);
+    $aceH = array("value" => 1, "img" => "<img src=\"CardImgs/Hearts/aceH.png\" alt=\"CardImage\">",);
+    $aceD = array("value" => 1, "img" => "<img src=\"CardImgs/Diamonds/aceD.png\" alt=\"CardImage\">",);    
+    $aceS = array("value" => 1, "img" => "<img src=\"CardImgs/Spades/aceS.png\" alt=\"CardImage\">",);
+    $aceC = array("value" => 1, "img" => "<img src=\"CardImgs/Clubs/aceC.png\" alt=\"CardImage\">",);
     
 
     //Hearts, Diamonds, Spades, Clubs
@@ -159,94 +159,85 @@
         }
     }
        function draw(){
+        global $players, $deck, $kingH, $kingD, $kingS, $kingC,$queenH, $queenD, $queenS, $queenC,$jackH, $jackD, $jackS, $jackC,
+        $tenH, $tenD, $tenS, $tenC,
+        $nineH, $nineD, $nineS, $nineC,$eightH, $eightD, $eightS, $eightC,$sevenH, $sevenD, $sevenS, $sevenC,$sixH, $sixD, $sixS, $sixC,$fiveH, $fiveD, $fiveS, $fiveC,$fourH, $fourD, $fourS, $fourC,$threeH, $threeD, $threeS, $threeC,$twoH, $twoD, $twoS, $twoC,$aceH, $aceD, $aceS, $aceC;
+        global $winner, $me, $comp1, $comp2, $comp3;
         echo "<div class = \"board\">
         <div class = \"title\">
             <h2>Silver Jack</h2>
         </div>
         <div class = \"allPlayers\">
             <div class=\"player\">
-                <div class=\"playerImage\">
-                <img src=\"PlayerImgs/player1.jpg\" alt=\"PlayerImage\">
-                </div>
-                <div class=\"hand\">
-                 <img src=\"CardImgs/Clubs/aceC.png\" alt=\"CardImage\">
-                  <img src=\"CardImgs/Clubs/twoC.png\" alt=\"CardImage\">
-                   <img src=\"CardImgs/Clubs/threeC.png\" alt=\"CardImage\">
-                    <img src=\"CardImgs/Clubs/fourC.png\" alt=\"Cardmage\">
-                     <img src=\"CardImgs/Clubs/fiveC.png\" alt=\"CardImage\">
-                      <img src=\"CardImgs/Clubs/sixC.png\" alt=\"CardImage\">
+                <div class=\"playerImage\">";
+                echo $players[0]["img"];
+               echo" </div>
+                <div class=\"hand\">";
+                for($i = 0; $i < sizeof($players[0]["hand"]); $i++){
+                    echo $players[0]["hand"][$i]["img"];
+                }
+            echo"
                 </div>
                 <div class=\"points\">
-                <br>
-                SCORE?
-                </div>
+                <br>";
+                echo $players[0]["points"];
+                echo"</div>
                 <div class=\"result\">
                 <br>
-                Winner!
+                
                 </div>
             </div>
             <div class=\"player\">
-                <div class=\"playerImage\">
-                <img src=\"PlayerImgs/player2.jpg\" alt=\"PlayerImage\">
-                </div>
-                     <div class=\"hand\">
-                 <img src=\"CardImgs/Clubs/aceC.png\" alt=\"CardImage\">
-                  <img src=\"CardImgs/Clubs/twoC.png\" alt=\"CardImage\">
-                   <img src=\"CardImgs/Clubs/threeC.png\" alt=\"CardImage\">
-                    <img src=\"CardImgs/Clubs/fourC.png\" alt=\"Cardmage\">
-                     <img src=\"CardImgs/Clubs/fiveC.png\" alt=\"CardImage\">
-                      <img src=\"CardImgs/Clubs/sixC.png\" alt=\"CardImage\">
-                </div>
+                <div class=\"playerImage\">";
+                echo $players[1]["img"];
+                echo"</div>
+                     <div class=\"hand\">";
+                  for($i = 0; $i < sizeof($players[1]["hand"]); $i++){
+                    echo $players[1]["hand"][$i]["img"];
+                }
+               echo" </div>
                 <div class=\"points\">
-                <br>
-                SCORE?
-                </div>
+                <br>";
+                echo $players[1]["points"];
+                echo"</div>
                 <div class=\"result\">
                 <br>
-                Winner!
+                
                 </div>
             </div>
             <div class=\"player\">
-                <div class=\"playerImage\">
-                <img src=\"PlayerImgs/player3.jpg\" alt=\"PlayerImage\">
-                </div>
-                     <div class=\"hand\">
-                 <img src=\"CardImgs/Clubs/aceC.png\" alt=\"CardImage\">
-                  <img src=\"CardImgs/Clubs/twoC.png\" alt=\"CardImage\">
-                   <img src=\"CardImgs/Clubs/threeC.png\" alt=\"CardImage\">
-                    <img src=\"CardImgs/Clubs/fourC.png\" alt=\"Cardmage\">
-                     <img src=\"CardImgs/Clubs/fiveC.png\" alt=\"CardImage\">
-                      <img src=\"CardImgs/Clubs/sixC.png\" alt=\"CardImage\">
-                </div>
+                <div class=\"playerImage\">";
+                echo $players[2]["img"];
+                echo"</div>
+                     <div class=\"hand\">";
+                  for($i = 0; $i < sizeof($players[2]["hand"]); $i++){
+                    echo $players[2]["hand"][$i]["img"];
+                }
+                echo"</div>
                 <div class=\"points\">
-                <br>
-                SCORE?
-                </div>
+                <br>";
+                echo $players[2]["points"];
+                echo"</div>
                 <div class=\"result\">
                 <br>
-                Winner!
+               
                 </div>
             </div>
              <div class=\"player\">
-                <div class=\"playerImage\">
-                <img src=\"PlayerImgs/player4.jpg\" alt=\"PlayerImage\">
-                </div>
-                     <div class=\"hand\">
-                 <img src=\"CardImgs/Clubs/aceC.png\" alt=\"CardImage\">
-                  <img src=\"CardImgs/Clubs/twoC.png\" alt=\"CardImage\">
-                   <img src=\"CardImgs/Clubs/threeC.png\" alt=\"CardImage\">
-                    <img src=\"CardImgs/Clubs/fourC.png\" alt=\"Cardmage\">
-                     <img src=\"CardImgs/Clubs/fiveC.png\" alt=\"CardImage\">
-                      <img src=\"CardImgs/Clubs/sixC.png\" alt=\"CardImage\">
-                </div>
+                <div class=\"playerImage\">";
+                echo $players[3]["img"];
+                echo"</div>
+                     <div class=\"hand\">";
+                for($i = 0; $i < sizeof($players[3]["hand"]); $i++){
+                    echo $players[3]["hand"][$i]["img"];
+                }
+                echo"</div>
                 <div class=\"points\">
-                <br>
-                500
-                </div>
+                <br>";
+               echo $players[3]["points"];
+                echo" </div>
                 <div class=\"result\">
-                <br>
-               player 1 wins!
-                </div>
+                <br></div>
             </div>
             <div>
                 
@@ -260,11 +251,11 @@
     }
 
     function startGame(){
-        global $winner;
+        global $winner, $players;
+        shuffle($players);
         shuffleDeck();
         dealAll();
         whoWins();
-        var_dump($winner);
     }
     startGame();
     draw();
